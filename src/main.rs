ca98f06372f67b3af834bd19997bc718a5c42d2c
tieje <toj320@gmail.com>
2023-12-18 12:12:02 -0500

    askama, axum, and htmx integration completed

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
-use axum::{
-    http::{header::CONTENT_TYPE, Method, StatusCode},
-    routing::{get, post},
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
-use tower_http::cors::{Any, CorsLayer};
+use askama::Template;
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::OPTIONS])
-        .allow_origin(Any)
-        .allow_headers([CONTENT_TYPE]);
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
-        .layer(cors);
+        .route("/", get(hello))
+        .route("/click", get(click));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "<p>Hello, World!</p>"
+#[derive(Template)]
+#[template(path = "pages/hello.html")]
+struct HelloTemplate;
+
+#[derive(Template)]
+#[template(path = "pages/click.html")]
+struct ClickTemplate;
+
+async fn hello() -> HelloTemplate {
+    HelloTemplate {}
+}
+async fn click() -> ClickTemplate {
+    ClickTemplate {}
 }
